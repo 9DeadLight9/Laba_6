76dc0ff40948fb7d5c895295b313bd7bf774cc0b
Anatoly Blinda <74497780+9DeadLight9@users.noreply.github.com>
2023-12-17 21:03:02 +0200

    Update Calculator.cs

--- Tusk_1/Calculator.cs
+++ Tusk_1/Calculator.cs
@@ -1,6 +1,34 @@
-﻿namespace Laba_6;
+namespace Laba_6;
 
-public class Calculator
+public class Calculator<T>
 {
-    
+    public delegate T Operation(T a, T b);
+
+    public T Add(T a, T b)
+    {
+        dynamic x = a, y = b;
+        return x + y;
+    }
+
+    public T Subtract(T a, T b)
+    {
+        dynamic x = a, y = b;
+        return x - y;
+    }
+
+    public T Multiply(T a, T b)
+    {
+        dynamic x = a, y = b;
+        return x * y;
+    }
+
+    public T Divide(T a, T b)
+    {
+        dynamic x = a, y = b;
+        if (y == 0)
+        {
+            throw new DivideByZeroException();
+        }
+        return x / y;
+    }
 }
