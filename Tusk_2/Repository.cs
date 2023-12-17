45b398a936a4329431238e2f7bf03c6234dbb9de
Anatoly Blinda <74497780+9DeadLight9@users.noreply.github.com>
2023-12-17 21:04:03 +0200

    Update Repository.cs

--- Tusk_2/Repository.cs
+++ Tusk_2/Repository.cs
@@ -1,6 +1,20 @@
-﻿namespace Tusk_2;
+namespace Tusk_2;
+using System.Collections.Generic;
+using System.Linq;
 
-public class Repository
+public delegate bool Criteria<T>(T item);
+
+public class Repository<T>
 {
-    
+    private List<T> items = new List<T>();
+
+    public void Add(T item)
+    {
+        items.Add(item);
+    }
+
+    public IEnumerable<T> Find(Criteria<T> criteria)
+    {
+        return items.Where(criteria.Invoke);
+    }
 }
