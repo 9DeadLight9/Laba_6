8e01a89ce66a19e60e5925ed71f25cbad0b95666
Anatoly Blinda <74497780+9DeadLight9@users.noreply.github.com>
2023-12-17 21:04:18 +0200

    Update Program.cs

--- Tusk_4/Program.cs
+++ Tusk_4/Program.cs
@@ -1,6 +1,22 @@
-﻿namespace Tusk_3;
+using Tusk_4;
 
-public class Program
+class Program
 {
+    static void Main()
+    {
+        var scheduler = new TaskScheduler<string, int>();
+
+        
+        scheduler.AddTask("Task1", 1);
+        scheduler.AddTask("Task2", 2);
+        scheduler.AddTask("Task3", 3);
+
     
+        TaskExecution<string> execution = task => Console.WriteLine($"Executing {task}");
+
+     
+        scheduler.ExecuteNext(execution);
+        scheduler.ExecuteNext(execution);
+        scheduler.ExecuteNext(execution);
+    }
 }
