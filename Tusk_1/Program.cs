39b9d136925c9a488463af4500b1d22499c3fdb2
Anatoly Blinda <74497780+9DeadLight9@users.noreply.github.com>
2023-12-17 21:03:12 +0200

    Update Program.cs

--- Tusk_1/Program.cs
+++ Tusk_1/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+using Laba_6;
+class Program
+{
+    static void Main()
+    {
+        Calculator<int> intCalc = new Calculator<int>();
+        Console.WriteLine(intCalc.Add(5, 10));
+        Console.WriteLine(intCalc.Subtract(20, 5));
+        Console.WriteLine(intCalc.Multiply(3, 7));
+        Console.WriteLine(intCalc.Divide(15, 3));
 
-Console.WriteLine("Hello, World!");
+        Calculator<double> doubleCalc = new Calculator<double>();
+        Console.WriteLine(doubleCalc.Add(5.5, 10.2));
+        Console.WriteLine(doubleCalc.Subtract(20.8, 5.3));
+        Console.WriteLine(doubleCalc.Multiply(3.2, 7.7));
+        Console.WriteLine(doubleCalc.Divide(15.6, 3.1));
+    }
+}
+
