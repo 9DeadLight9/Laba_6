da47ef85fbe61dbe210d035259d080e78cf80c84
Anatoly Blinda <74497780+9DeadLight9@users.noreply.github.com>
2023-12-17 21:03:49 +0200

    Update Program.cs

--- Tusk_2/Program.cs
+++ Tusk_2/Program.cs
@@ -1,1 +1,28 @@
-﻿
+using Tusk_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var repository = new Repository<int>();
+
+     
+        repository.Add(1);
+        repository.Add(2);
+        repository.Add(3);
+        repository.Add(4);
+        repository.Add(5);
+
+   
+        Criteria<int> criteria = item => item % 2 == 0;
+
+       
+        var result = repository.Find(criteria);
+
+      
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
